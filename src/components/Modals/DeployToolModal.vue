a3bc4415831ca0802590e26df597c4e6c09913d0
Josh Garnick <josh@limelightdept.com>
2021-12-01 09:13:11 -0800

    adding some styles for deploy tool modal

--- src/components/Modals/DeployToolModal.vue
+++ src/components/Modals/DeployToolModal.vue
@@ -1,20 +1,34 @@
 <template>
 	<Modal :valign="'top'" style="z-index:999" :size="'creation'" :open="open">
-		<h1 class="mb-2 headline">Select a Tool to Deploy</h1>
+		<h1 class="headline">Deploy A Tool</h1>
+		<div class="mb-10">
+			<span class="headline-2">Select a Tool to Continue</span>
+		</div>
 		
-		<div v-if="myTeam == 1 || myTeam == 2">
-			<!-- <button class="mr-2 text-center text-pallette-button hover:text-pallette-button-hover" @click.prevent="EventBus.openModal( ()=>{ EventBus.$emit('deployToolModal', false); EventBus.$emit('deployWCAGAuditModal', true); })">
-				<div><i class="far fa-wrench"></i></div>
-				WCAG Audit
-			</button> -->
-			<Button @click.native.prevent="EventBus.openModal( ()=>{ EventBus.$emit('deployToolModal', false); EventBus.$emit('deployWCAGAuditModal', true); })" class="mr-2 text-center">
-				<div><i class="far fa-wrench"></i></div>
-				WCAG Audit
-			</Button>
-			<Button @click.native.prevent="EventBus.openModal( ()=>{ EventBus.$emit('deployToolModal', false); EventBus.$emit('deployMediaAuditModal', true); })" class="mr-2 text-center">
-				<div><i class="far fa-photo-video"></i></div>
-				Media Audit
-			</Button>
+		<div class="flex justify-evenly items-start" v-if="myTeam == 1 || myTeam == 2">
+			<button 
+			class="font-bold mr-2 text-center text-pallette-button hover:text-pallette-button-hover" 
+			@click.prevent="EventBus.openModal( ()=>{ EventBus.$emit('deployToolModal', false); EventBus.$emit('deployWCAGAuditModal', true); })">
+				<div class="mb-5 text-6xl"><i class="far fa-th-list"></i></div>
+				<span class="barlow text-xl">WCAG Audit</span>
+			</button>
+			
+			<button 
+			class="font-bold mr-2 text-center text-pallette-button hover:text-pallette-button-hover" 
+			@click.prevent="EventBus.openModal( ()=>{ EventBus.$emit('deployToolModal', false); EventBus.$emit('deployMediaAuditModal', true); })">
+				<div class="mb-5 text-6xl"><i class="far fa-photo-video"></i></div>
+				<span class="barlow text-xl">Media Audit</span>
+			</button>
+			
+			<button 
+			class="font-bold mr-2 text-center text-pallette-button hover:text-pallette-button-hover" 
+			@click.prevent>
+				<div class="mb-5 text-6xl"><i class="far fa-palette"></i></div>
+				<span class="barlow text-xl">
+					<div>Color Swatch</div>
+					Analysis
+				</span>
+			</button>
 		</div>
 		<button @click.prevent="EventBus.closeModal( ()=>{ EventBus.$emit('deployToolModal', false)})" class="standard mt-2">Cancel</button>
 	</Modal>
